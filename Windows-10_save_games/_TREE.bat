9b1db37617a468422ad1d5a044e061d42bf1177b
Martin-Pavek <146820572+Martin-Pavek@users.noreply.github.com>
2023-11-08 13:09:44 +0100

    Add files via upload

---
+++ Windows-10_save_games/_TREE.bat
@@ -0,0 +1,7 @@
+@echo off
+date /t > C:\Users\DELL\Documents\zaloha\save_hry\_TREE_folder_save_hry.txt
+time /t >> C:\Users\DELL\Documents\zaloha\save_hry\_TREE_folder_save_hry.txt
+tree /F /A C:\Users\DELL\Documents\zaloha\save_hry\ >> C:\Users\DELL\Documents\zaloha\save_hry\_TREE_folder_save_hry.txt
+@echo on
+
+
